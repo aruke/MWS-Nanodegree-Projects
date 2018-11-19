8978e1c6513fcc0c5dc7912504e78c49c1cdc6f0
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2018-11-19 23:28:51 +0900

    Update a11y and responsiveness

--- Restaurant Reviews App Stage-1/js/main.js
+++ Restaurant Reviews App Stage-1/js/main.js
@@ -151,6 +151,8 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = restaurant.name + ' Restaurant';
+    image.alt = restaurant.name + ' Restaurant';
     element.append(image);
 
     const name = document.createElement('h1');
