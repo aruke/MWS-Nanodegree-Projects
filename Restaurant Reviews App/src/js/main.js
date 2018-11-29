1ec236dadb429c67c934aec6cd77510e0705e3e3
Rajanikant Deshmukh <rajanikant.deshmukh@quipper.com>
2018-11-30 01:35:00 +0900

    Add gulp and update project structure

--- Restaurant Reviews App/src/js/main.js
+++ Restaurant Reviews App/src/js/main.js
@@ -200,7 +200,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
  */
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
+        navigator.serviceWorker.register('service-worker.js', {scope: '/'})
             .then(function () {
                 console.log("ServiceWorker registered.");
             }).catch(function () {
