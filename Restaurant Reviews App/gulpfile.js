1a61a6a705ab3c280657efe3062b2cd16a94bfd6
Rajanikant Deshmukh <rajanikant.deshmukh@quipper.com>
2018-12-04 23:46:38 +0900

    Add build tasks

--- Restaurant Reviews App/gulpfile.js
+++ Restaurant Reviews App/gulpfile.js
@@ -1,13 +1,47 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 
+gulp.task('html', function(){
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build/'))
+});
+
+gulp.task('css', function(){
+    return gulp.src('src/css/*.css')
+        .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('js', function(){
+    return gulp.src('src/js/*.js')
+        .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('img', function(){
+    return gulp.src('src/img/**')
+        .pipe(gulp.dest('build/img/'))
+});
+
+gulp.task('ico', function(){
+    return gulp.src('src/ico/**')
+        .pipe(gulp.dest('build/ico/'))
+});
+
+gulp.task('pwa', function(){
+    return gulp.src('src/pwa/**')
+        .pipe(gulp.dest('build/pwa/'))
+});
+
+gulp.task('sw', function(){
+    return gulp.src('src/service-worker.js')
+        .pipe(gulp.dest('build/'))
+});
+
+gulp.task('build', gulp.parallel('html', 'css', 'img', 'ico', 'js', 'pwa', 'sw'));
+
 gulp.task('connect', function () {
     connect.server({
-        root: 'src',
-        livereload: true
+        root: 'build'
     })
 });
 
-gulp.task('default', gulp.parallel('connect'), function() {
-    console.log("Default task finished.")
-});
+gulp.task('default', gulp.series('build', 'connect'));
