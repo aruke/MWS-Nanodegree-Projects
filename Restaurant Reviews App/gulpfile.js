aa4113936f3f9018ea21d8e8aa028bc0b64eec0f
Rajanikant Deshmukh <rajanikant.deshmukh@quipper.com>
2018-12-06 02:52:09 +0900

    Add browser-sync and beautify gulpfile

--- Restaurant Reviews App/gulpfile.js
+++ Restaurant Reviews App/gulpfile.js
@@ -1,51 +1,92 @@
 let gulp = require('gulp');
-let connect = require('gulp-connect');
 let sass = require('gulp-sass');
 let concatCss = require('gulp-concat-css');
 
-gulp.task('html', function(){
-    return gulp.src('src/*.html')
+let browserSync = require('browser-sync').create();
+
+/**
+ * Path constants for sources and destinations.
+ * @type {{html: {src: string, dest: string}, scss: {src: string[], dest: string}}}
+ */
+let paths = {
+    html: {
+        src: 'src/*.html',
+        dest: 'build/'
+    },
+    scss: {
+        src: ['node_modules/materialize-css/sass/**/*.scss', 'src/sass/*.scss'],
+        dest: 'build/css/'
+    },
+    js: {
+        src: 'src/js/*.js',
+        dest: 'build/js'
+    }
+};
+
+/**
+ * Constants for task names.
+ * @type {{html: string, scss: string}}
+ */
+let tasks = {
+    html: 'html',
+    scss: 'scss',
+    js: 'js'
+};
+
+/**
+ * HTML task.
+ */
+gulp.task(tasks.html, function () {
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dest))
+
+});
+
+/**
+ * SCSS to CSS task.
+ */
+gulp.task(tasks.scss, function () {
+    return gulp.src(paths.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concatCss("styles.css"))
+        .pipe(gulp.dest(paths.scss.dest))
+        .pipe(browserSync.stream())
+
+});
+
+/**
+ * JS task
+ */
+gulp.task(tasks.js, function () {
+    return gulp.src(paths.js.src)
+        .pipe(gulp.dest(paths.js.dest))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.watch(paths.js.src, gulp.series(tasks.js));
+
+gulp.task('assets', function () {
+    return gulp.src('src/assets/**/*')
         .pipe(gulp.dest('build/'))
 });
 
-gulp.task('sass', function(){
-    return gulp.src(['node_modules/materialize-css/sass/**/*.scss', 'src/sass/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concatCss("styles.css"))
-        .pipe(gulp.dest('build/css/'))
-});
-
-gulp.task('js', function(){
-    return gulp.src('src/js/*.js')
-        .pipe(gulp.dest('build/js'))
-});
-
-gulp.task('img', function(){
-    return gulp.src('src/img/**')
-        .pipe(gulp.dest('build/img/'))
-});
-
-gulp.task('ico', function(){
-    return gulp.src('src/ico/**')
-        .pipe(gulp.dest('build/ico/'))
-});
-
-gulp.task('pwa', function(){
-    return gulp.src('src/pwa/**')
-        .pipe(gulp.dest('build/pwa/'))
-});
-
-gulp.task('sw', function(){
+gulp.task('sw', function () {
     return gulp.src('src/service-worker.js')
         .pipe(gulp.dest('build/'))
 });
 
-gulp.task('build', gulp.parallel('html', 'sass', 'img', 'ico', 'js', 'pwa', 'sw'));
+gulp.task('build', gulp.parallel(tasks.html, tasks.scss, tasks.js, 'assets', 'sw'));
 
-gulp.task('connect', function () {
-    connect.server({
-        root: 'build'
-    })
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'build'
+        }
+    });
+
+    gulp.watch(paths.html.src, gulp.series(tasks.html));
+    gulp.watch(paths.scss.src, gulp.series(tasks.scss));
+    gulp.watch(paths.js.src, gulp.series(tasks.js));
 });
 
-gulp.task('default', gulp.series('build', 'connect'));
+gulp.task('default', gulp.series('build', 'browserSync'));
